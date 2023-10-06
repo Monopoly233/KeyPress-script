a08d28abe24cd50ae763eb5a70b2b2b5aa941192
Monopoly233 <63429982+Monopoly233@users.noreply.github.com>
2023-10-05 21:55:16 -0700

    Add files via upload

--- keypress.py
+++ keypress.py
@@ -1,27 +1,27 @@
 import keyboard
 import time
 
-# 设置按住的键位
+# set the pressed key
 key_to_hold = 'j'
 
-# 设置连续输出次数
+# set the number of time output
 output_count = 20
 
-# 设置连续输出的时间间隔（秒）
+#the interval between each output
 output_interval = 0.00001
 
-# 提示用户开始
-print(f"按下键位'{key_to_hold}'后，将连续输出{output_count}次。")
+# remind the user
+print(f"after press '{key_to_hold}'，will constant ouput {output_count} times")
 keyboard.wait(key_to_hold)
 
 while True:
-    # 开始按住键位并连续输出
+    # press the key and release
     keyboard.press(key_to_hold)
     for i in range(output_count):
         keyboard.press_and_release(key_to_hold)
         time.sleep(output_interval)
     keyboard.release(key_to_hold)
 
-    print(f"按住键位'{key_to_hold}'并连续输出{output_count}次完成！")
-    print("等待下一次按下键位...")
+    print(f"after press '{key_to_hold}'，will constant ouput {output_count} times")
+    print("waiting for press...")
     keyboard.wait(key_to_hold)
